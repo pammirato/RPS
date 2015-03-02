9b8d9ef41ed7a6a7315da1a557531c376c182cbc
pammirato <pammirato10@aol.com>
2015-03-02 11:37:17 -0500

    Create randomPlayer.py

---
+++ randomPlayer.py
@@ -0,0 +1,16 @@
+#import statment for random
+
+
+
+
+#this line should have something with the random function
+
+		if ():
+			#something with output
+		elif:
+			#something with output
+		else:
+			#something with output
+
+			
+	#and thats it!
