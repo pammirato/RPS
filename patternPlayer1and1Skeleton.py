e5e7be169421725d5a60dde39dd95db598e9acba
pammirato <ammirato@cs.unc.edu>
2015-03-04 11:02:33 -0500

    pattern1and1

---
+++ patternPlayer1and1Skeleton.py
@@ -0,0 +1,82 @@
+import random
+
+
+#this player uses history to try to predict the opponents
+#next move.
+
+#Remember the input variable is given to us with our opponents
+#last move. We will find the last time they made the same throw,
+#and then throw what will beat whatever they threw next.
+
+#Example.  Input = "R" - our opponent just threw "R"
+#			We will look  through the history(oldThrows)
+#			For the last time they threw "R". Then we will
+#			look at what they threw next, and throw to beat that.
+
+#Example 2. Suppose After a few rounds oldThrows =[ "R", "R", ,"P", "S", "S", "R"]
+#			Then in the next rpound, we get input="P"
+#			So we will look through oldThrows to get the largest index of "P"
+#   		Then look at the move after that, "S" in this case. 
+#			We will throw "R", since that beats "S".
+#			The idea is that since the opponent threw "S" after paper last time,
+#			maybe they will do it again.
+
+
+
+#Hwe still store our opponents moves in oldThrows 
+
+
+
+
+#in the first round input is empty!
+if not input:
+	#so initialize the list, and pick a random throw
+	oldThrows = []
+	output = random.choice(["R","P", "S"])
+
+#for every turn but the first we go here	
+else:
+	
+	lastIndex = getLastIndexOf(oldThrows, *REPLACE*)
+
+	if(lastIndex == *REPLACE*):
+		output = random.choice(["R","P", "S"])
+
+	else:
+		throwToBeat = oldThrows[*REPLACE*]
+
+		output = throwThatBeatsThis(*REPLACE*)
+
+		
+	oldThrows.append(*REPLACE*)
+
+
+
+
+
+
+
+#gets the last index of item in array
+def getLastIndexOf(array, item):
+
+	lastIndex = -1;
+
+	#i ranges over all indices of the array
+	for i in range(1,*REPLACE*):
+
+		if *REPLACE*:
+			lastIndex = i
+
+	return lastIndex
+
+#end of getLastIndexOf
+
+
+
+#returns the throw that will beat throwToBeat
+def throwThatBeatsThis(throwToBeat):
+	*REPLACE*
+	#theres a bunch ofl lines to write here
+
+
+#end throwThatBeatsThis
